d9ed14a56d05c3555bfe9c89359c6651251842d7
Roman Strakhov <roman.strakhov@alberblanc.com>
2023-08-08 21:09:45 +0400

    add convenience tests

--- test/ScrambleProcessingTests.cpp
+++ test/ScrambleProcessingTests.cpp
@@ -12,6 +12,10 @@ TEST(ConvenienceScore, Basic) {
         {"R B", "R Bw"}, // Bw is worse than B
         {"U R F D'", "U R F D2"}, // double is worse
         {"Rw' S L", "R U R' U'"}, // more moves -> worse
+        {"Bw2", "F2 z2"}, // there's no way F2 z2 is better than the equivalent wide move
+        {"Fw2", "Bw2"}, // F move should be better
+        {"F2", "Fw2"}, // non-wide move should be better
+        {"S'", "S2"}, // single move is always better than double
     };
     for (const auto& [good, worse] : good_and_worse_pairs) {
         auto good_score = execution_convenience_score(good);
